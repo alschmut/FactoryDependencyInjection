744843bcd7aa687a405cc9c7a08132bcbadd67c7
Alexander Schmutz <alexander.schmutz@maibornwolff.de>
2024-02-13 11:14:59 +0100

    Initial Commit

---
+++ FactoryDependencyInjection/FactoryDependencyInjectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FactoryDependencyInjectionApp.swift
+//  FactoryDependencyInjection
+//
+//  Created by Alexander Schmutz on 13.02.24.
+//
+
+import SwiftUI
+
+@main
+struct FactoryDependencyInjectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
